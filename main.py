b829549eebc9c8dbd8c849a636e40776a67ff2c3
eangcms <93700456+eangcms@users.noreply.github.com>
2024-10-27 19:09:38 +0800

    Remove unnecessary code and dependencies

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# coding=utf-8
-# Copyright 2020 The Google Research Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Training and evaluation"""
-
 import run_lib
 from absl import app
 from absl import flags
@@ -22,8 +5,11 @@ from ml_collections.config_flags import config_flags
 import logging
 import os
 import tensorflow as tf
+import torch
 
 FLAGS = flags.FLAGS
+
+device = torch.device("cpu")
 
 config_flags.DEFINE_config_file(
   "config", None, "Training configuration.", lock_config=True)
